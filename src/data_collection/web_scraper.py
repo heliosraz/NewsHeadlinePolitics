ed4969e76477223c21780874b4b73d05c026e77f
Sunny Zhou <sutzhou@ucdavis.edu>
2022-11-29 01:05:58 -0800

    update webscraper and finish cnn scraping

--- src/data_collection/web_scraper.py
+++ src/data_collection/web_scraper.py
@@ -3,7 +3,7 @@ from bs4 import BeautifulSoup
 import re
 
 #create base url and cases
-base_url = "https://www.cnn.com/"
+base_url = "https://www.cnn.com"
 url = "https://www.cnn.com/sitemap.html"
 page = requests.get(url)
 site=[]
@@ -24,23 +24,41 @@ for link in year_links:
 #loop through years to collect data
 for ytext in year_hrefs:
     month_hrefs=[]
-    url=base_url+str(ytext)
-    page = requests.get(url)
+    #generate year_url
+    year_url=base_url+str(ytext)
+    year_page = requests.get(year_url)
     # get all month urls per year
-    month_block=soup.find("ul", class_="sitemap-month")
+    year_soup = BeautifulSoup(year_page.content, "html.parser")
+    # print(ytext)
+    # print(year_soup)
+    month_block=year_soup.find("ul", class_="sitemap-month")
+    # print(month_block)
     month_links=month_block.find_all("a")
 
     for link in month_links:
         href=link.get("href")
         month_hrefs.append(str(href))
     #loop through months to collect data
-    for mtext in month_href:
-        headline_block=soup.find("li")
-        headline_date.append(headline_block.find("span",class_="date").text)
-        headline.append(headline_block.find("span",class_="sitemap-link").find("a").text)
-#write data into csv    
-headline_data=open('./date/headlines.csv', 'w')
-headline_dates_data=open('./date/headlines_dates.csv', 'w')
+    for mtext in month_hrefs:
+        #generate month_url
+        month_url=base_url+str(mtext)
+        month_page = requests.get(month_url)
+        # get all month urls per year
+        month_soup = BeautifulSoup(month_page.content, "html.parser")
+        # print(month_soup)
+        headline_block=month_soup.find_all("div", class_="sitemap-entry")[1]
+        # print(headline_block)
+
+        indv_headlines=headline_block.find_all("li")
+        for item in indv_headlines:
+            print(f'{str(item.find("span",class_="date").string)},{str(item.find("span",class_="sitemap-link").string)}')
+            d=headline_date.append(str(item.find("span",class_="date").string))
+            h=headline.append(str(item.find("span",class_="sitemap-link").string))
+#write data into csv
+headline_data=open('headlines.csv', 'w')
+headline_dates_data=open('headlines_dates.csv', 'w')
+headline_data.write("CNN\n")
+headline_dates_data.write("CNN\n")
 for index in range(len(headline)):
     headline_data.write(headline[index]+"\n")
     headline_dates_data.write(headline_date[index]+"\n")
