f372e85e5c52913a124c5ad53418bebe97403845
Sunny Zhou <sutzhou@ucdavis.edu>
2022-12-08 23:26:32 -0800

    main.py initialized and renamed notebook

---
+++ src/main.py
@@ -0,0 +1,16 @@
+#import packages
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy import linalg as LA
+import plotly
+import plotly.graph_objects as go
+import random, time
+from collections import Counter
+from sklearn.model_selection import train_test_split
+import json
+from sklearn.datasets import fetch_openml
+import json
+from json import JSONEncoder
+
+#defining possible activations
+#term document matrix generation
