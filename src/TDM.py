02c9bf3f8bdd7885119e7b46e5681cdb6026b347
Maxi1800 <92944189+Maxi1800@users.noreply.github.com>
2022-12-09 11:52:18 -0800

    add TDM file

---
+++ src/TDM.py
@@ -0,0 +1,30 @@
+import os
+import csv
+import random
+import pandas as pd
+from pathlib import Path
+
+# Finds path to data folder
+p = Path(__file__).parents[1]
+data = str(p) + "/data/data_collection/"
+
+# Sets up variables
+all_words = []
+
+# Input parameters
+samples = 1000
+
+ 
+# iterate over files in that directory
+for filename in os.listdir(data):
+    file = os.path.join(data, filename)
+    #rows = csv.reader(open('foo.csv', 'rU'),delimiter=',')
+    
+    csvfile = pd.read_csv('/your/file/path/here')
+    row_count = sum(1 for row in open(data))
+    skip = sorted(random.sample(range(row_count),row_count-samples))
+    df = pd.read_csv(data, skiprows=skip)
+    
+    print(df)
+    # 
+    
