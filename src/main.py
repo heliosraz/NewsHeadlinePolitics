264ba65348d824384844a50c38908416190c10f6
Sunny Zhou <sutzhou@ucdavis.edu>
2022-12-09 01:07:32 -0800

    added comments

--- src/main.py
+++ src/main.py
@@ -3,7 +3,6 @@ import numpy as np
 import matplotlib.pyplot as plt
 from numpy import linalg as LA
 import plotly
-import plotly.graph_objects as go
 import random, time
 from collections import Counter
 from sklearn.model_selection import train_test_split
@@ -12,5 +11,16 @@ from sklearn.datasets import fetch_openml
 import json
 from json import JSONEncoder
 
-#defining possible activations
-#term document matrix generation
+term_dict={}
+# methods
+##defining possible activations
+
+##adding words into dictionary
+##term document matrix generation
+
+## at some point need
+# U, S, V =np.linalg.svd(X)
+# X is the term doument matrix
+# U is the document-topic matrix
+# S is the diagonal matrix of sqrt(eigenvalues)
+# V is the word embedding matrix
