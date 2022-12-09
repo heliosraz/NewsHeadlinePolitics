0399648726f6cbbd3cfd6964e827918fcd595355
Maxi1800 <92944189+Maxi1800@users.noreply.github.com>
2022-12-09 12:16:29 -0800

    Update TDM.py
    update tdm

--- src/TDM.py
+++ src/TDM.py
@@ -6,7 +6,7 @@ from pathlib import Path
 
 # Finds path to data folder
 p = Path(__file__).parents[1]
-data = str(p) + "/data/data_collection/"
+data = str(p) + "/data/data-collection/raw-data"
 
 # Sets up variables
 all_words = []
@@ -17,13 +17,13 @@ samples = 1000
  
 # iterate over files in that directory
 for filename in os.listdir(data):
+    print(data)
     file = os.path.join(data, filename)
     #rows = csv.reader(open('foo.csv', 'rU'),delimiter=',')
     
     csvfile = pd.read_csv('data')
-    row_count = sum(1 for row in open(data))
+    row_count = sum(1 for row in csv.reader(data))
     skip = sorted(random.sample(range(row_count),row_count - samples))
     df = pd.read_csv(data, skiprows=skip)
 
     print(df)
-    # 
